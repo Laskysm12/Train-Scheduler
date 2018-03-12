3f379747a9d48ff0a19b88cbc9a0b2a8e2f62ad0
Sam Lasky <Laskysm@gmail.com>
2018-03-12 16:32:01 -0400

    second commit

---
+++ assets/javascript/logic.js
@@ -0,0 +1,15 @@
+var config = {
+    apiKey: "AIzaSyCrWL1JaBSjbXPvyIUL_hmiriFyLhjYYs8",
+    authDomain: "trainscheduler-a6988.firebaseapp.com",
+    databaseURL: "https://trainscheduler-a6988.firebaseio.com",
+    projectId: "trainscheduler-a6988",
+    storageBucket: "trainscheduler-a6988.appspot.com",
+    messagingSenderId: "261117632750"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
+// Need to add (event)preventDefault
