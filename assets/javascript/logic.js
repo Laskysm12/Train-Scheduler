995f905938d3051201afec08484a8e3bbf3ec3a1
Sam Lasky <Laskysm@gmail.com>
2018-03-12 17:28:04 -0400

    saving progress

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -8,8 +8,41 @@ var config = {
   };
   firebase.initializeApp(config);
 
+var database = firebase.database();
 
+// Button for adding Trains
+$("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+})
 
+// Grabs user input
+var trainName = $("#train-name-input").val().trim();
+var trainDestination = $("#destination-input").val().trim();
+var trainFirst = $("#first-train-input").val().trim(); // NEED TO ADD moment info here
+var trainFrequency = $("#frequency-input").val().trim();
 
+// Creates local "temporary" object for holding train data
+var trainData = {
+    name: trainName,
+    destination: trainDestination,
+    first: trainFirst,
+    frequency: trainFrequency
+    // Will need to add a variable for minutes away!!!
+};
 
-// Need to add (event)preventDefault
+// Upload train data to the database
+database.ref().push(trainData);
+
+// Logs everything to the console
+console.log(trainData.name);
+console.log(trainData.destination);
+console.log(trainData.first);
+console.log(trainData.frequency);
+//Need to add one for minutes away!
+
+// Clears all of the text-boxes
+$("#train-name-input").val("");
+$("#destination-input").val("");
+$("#first-train-input").val("");
+$("#frequency-input").val("");
+//Need to add one for minutes away!
